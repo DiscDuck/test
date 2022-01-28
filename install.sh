dcd3157e7044d6a26426db8954f055375f923442
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-01-28 13:06:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # License : General Public License GPL-3.0-or-later
 ######################################################################
 
-source /opt/discduck/funtions.sh
+source /opt/discduck/functions.sh
 
 clear
 msgbox "Test complete"
